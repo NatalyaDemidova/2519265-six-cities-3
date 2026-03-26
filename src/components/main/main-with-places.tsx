28c3fed45f5bff87e16d39726d8c527afa5bf97b
NatalyaDemidova <demidovanatalia691991@gmail.com>
2026-03-26 16:38:56 +0300

    module5-task2

--- src/components/main/main-with-places.tsx
+++ src/components/main/main-with-places.tsx
@@ -3,24 +3,30 @@ import { OfferForCardType } from '../../mosks/types/offer';
 import Map from '../map/map';
 import Card from './card';
 import Sorting from './sorting';
+import { CityType } from '../../mosks/types/city';
+import { DEFAULT_CITY } from '../../const';
 
 type MainScreenProps = {
   offersCard: OfferForCardType[];
+  city: CityType | null;
 };
-
-const CITY = 'Amsterdam';
 
 export default function MainWithPlaces({
   offersCard,
+  city
 }: MainScreenProps): JSX.Element {
   const [currentCardId, setCurrentCardId] = useState<string | null>(null);
+
+  if(!city) {
+    city = DEFAULT_CITY;
+  }
 
   return (
     <div className="cities__places-container container">
       <section className="cities__places places">
         <h2 className="visually-hidden">Places</h2>
         <b className="places__found">
-          {offersCard.length} places to stay in Amsterdam
+          {offersCard.length} places to stay in {city.name}
         </b>
         <Sorting />
         <div className="cities__places-list places__list tabs__content">
@@ -30,7 +36,7 @@ export default function MainWithPlaces({
         </div>
       </section>
       <div className="cities__right-section">
-        <Map offersCard={offersCard} city={CITY} currentCardId={currentCardId} />
+        <Map offersCard={offersCard} city={city} currentCardId={currentCardId} className='cities__map map' />
       </div>
     </div>
   );
